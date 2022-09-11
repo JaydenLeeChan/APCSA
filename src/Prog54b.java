e0699f43a72f0f5eb8d175a72da4e7b7538fb324
Jayden <chan.j@stu.janesville.k12.wi.us>
2022-09-11 15:47:06 -0500

    idk

--- src/Prog54b.java
+++ src/Prog54b.java
@@ -1,2 +1,19 @@
 public class Prog54b {
+    public static void main(String[] args) {
+        int numOne = 475;
+        int numTwo = 821;
+        int numThree = 369;
+        int numFour = 562;
+
+        int sum = numOne + numTwo + numThree + numFour;
+
+        double avg = sum/4.0;
+
+        System.out.println("The sum of the four numbers is " + sum);
+        System.out.println("The average of the four numbers is " + avg);
+
+
+
+
+    }
 }
