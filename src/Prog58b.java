0182d5fae331a73a7c8ebe5c82a98c3452ab9e5e
Jayden Chan <chan.j@stu.janesville.k12.wi.us>
2022-09-13 10:24:48 -0500

    Update

--- src/Prog58b.java
+++ src/Prog58b.java
@@ -1,7 +1,17 @@
-
+import java.util.*;
 
 public class Prog58b {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is A?");
+        int a = input.nextInt();
+        System.out.println("What is B?");
+        int b = input.nextInt();
+        System.out.println("What is C?");
+        int c = input.nextInt();
+
+        double root =
 
     }
 }
