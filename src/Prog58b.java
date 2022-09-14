fca34624193c3ae12e63a9e198a01340de9f9cea
Jayden Chan <chan.j@stu.janesville.k12.wi.us>
2022-09-14 10:22:59 -0500

    Update

--- src/Prog58b.java
+++ src/Prog58b.java
@@ -5,13 +5,26 @@ public class Prog58b {
         Scanner input = new Scanner(System.in);
 
         System.out.println("What is A?");
-        int a = input.nextInt();
+        double a = input.nextDouble();
         System.out.println("What is B?");
-        int b = input.nextInt();
+        double b = input.nextDouble();
         System.out.println("What is C?");
-        int c = input.nextInt();
+        double c = input.nextDouble();
 
-        double root =
+        double root1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2/a);
+        double root2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2/a);
 
+        System.out.println("The roots are: " + root1 + "," + root2);
+/*
+What is A?
+1
+What is B?
+5
+What is C?
+6
+The roots are: -2.0,-3.0
+
+Process finished with exit code 0
+ */
     }
 }
