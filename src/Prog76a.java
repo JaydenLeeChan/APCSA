5059f690f3299ba048c71953b570f75c4e798b20
jaydenchan <chan.j@stu.janesville.k12.wi.us>
2022-10-04 10:41:14 -0500

    k

--- src/Prog76a.java
+++ src/Prog76a.java
@@ -8,6 +8,23 @@ public class Prog76a {
         System.out.println("         " + bad);
         System.out.println("       X " + "9");
         System.out.println("__________");
-        System.out.println("        " + bad * 9);
+        String space = "        ";
+        if (bad * 9 < 9)
+            space = "         ";
+        System.out.println(space + bad * 9);
+        System.out.println("X 12345679");
+        System.out.println("__________");
+        System.out.print(bad * 9 * 12345679 + " Surprise!");
     }
 }
+/*
+Enter a number you dislike: 5
+         5
+       X 9
+__________
+        45
+X 12345679
+__________
+555555555 Surprise!
+Process finished with exit code 0
+ */
