d62bfd0089fe68509d2cbc5b037c153ba789a312
Jayden <chan.j@stu.janesville.k12.wi.us>
2022-10-07 04:21:18 -0500

    idk

---
+++ src/LP49.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class LP49 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 20: ");
+        int pl = input.nextInt();
+        int sec = (int)(Math.random() * (21 - 1) + 1);
+        System.out.println("Computer's Number: " + sec);
+        System.out.println("Player's Number: " + pl);
+        if (pl == sec) System.out.println("You won!");
+        else System.out.println("Better luck next time!");
+
+    }
+}
+/*
+Enter a number between 1 and 20: 14
+Computer's Number: 1
+Player's Number: 14
+Better luck next time!
+
+Process finished with exit code 0
+ */
