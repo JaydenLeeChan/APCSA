feecebcef8477042aaf50e89c14e584eb694131f
Jayden Chan <chan.j@stu.janesville.k12.wi.us>
2022-10-20 10:31:11 -0500

    Update

--- src/dfile.java
+++ src/dfile.java
@@ -6,6 +6,7 @@ public class dfile {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("data/prog285b.dat"));
+            //Put code here
         }
         catch (IOException e) {
             System.out.println("Can't find data file!");
