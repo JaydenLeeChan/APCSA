87222d87f5c4f339384f6c76905732140e410623
jaydenchan <chan.j@stu.janesville.k12.wi.us>
2023-01-11 10:14:02 -0600

    k

--- src/newstuff/Prog435a.java
+++ src/newstuff/Prog435a.java
@@ -17,6 +17,8 @@ public class Prog435a {
                 Cl435a bruh = new Cl435a(a, b);
                 bruh.setCar();
                 bruh.setGate();
+                System.out.println(bruh.getMycarType() + "\t$" + bruh.getMygateToll() + "\t"
+                + bruh.getMycarFactor() + "\t$" + bruh.getMycarFactor()*bruh.getMygateToll());
 
 
 
@@ -28,3 +30,23 @@ public class Prog435a {
         }
     }
 }
+/*
+Compact Car	$1.35	1.0	$1.35
+Small Care	$2.5	1.3	$3.25
+Mid Size Care	$4.1	1.6	$6.56
+Full Size Car	$5.5	2.0	$11.0
+Truck	$2.0	2.4	$4.8
+16 Wheeler	$3.25	2.7	$8.775
+Compact Car	$4.8	1.0	$4.8
+Small Care	$6.0	1.3	$7.800000000000001
+Mid Size Care	$1.35	1.6	$2.16
+Full Size Car	$2.5	2.0	$5.0
+Truck	$4.1	2.4	$9.839999999999998
+16 Wheeler	$5.5	2.7	$14.850000000000001
+Compact Car	$6.0	1.0	$6.0
+Small Care	$1.35	1.3	$1.7550000000000001
+Mid Size Care	$2.0	1.6	$3.2
+Full Size Car	$2.5	2.0	$5.0
+Truck	$3.25	2.4	$7.8
+16 Wheeler	$4.1	2.7	$11.07
+ */
