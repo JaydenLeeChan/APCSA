7e45f9d9459e265023c28c9a3ae5777b1aa9208b
jaydenchan <chan.j@stu.janesville.k12.wi.us>
2023-01-12 19:53:23 -0600

    k

---
+++ src/newstuff/Cl402a.java
@@ -0,0 +1,23 @@
+package newstuff;
+import java.io.*;
+import java.util.*;
+
+public class Cl402a {
+    private int myId = 0;
+    private int myScore = 0;
+
+    public Cl402a (int id, int score) {
+        myId = id;
+        myScore = score;
+
+    }
+
+    public int getMyId() { return myId; }
+    public int getMyScore() { return myScore; }
+
+
+}
+
+
+
+
---
+++ src/newstuff/Prog402a.java
@@ -0,0 +1,29 @@
+package newstuff;
+import java.io.*;
+import java.util.*;
+
+
+
+
+public class Prog402a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("data/p402a.dat"));
+            //Put code here
+
+            int[] idArray = new int[50];
+            int[] scoreArray = new int[50];
+
+            Scanner bruh = new Scanner(System.in);
+            for (int lcv = 0; bruh.hasNext(); lcv++) {
+
+            }
+
+
+
+        }
+        catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
+}
