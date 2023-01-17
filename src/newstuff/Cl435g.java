28d65e2de9aefea4a9abbdd197d204eb883f3f15
Jayden <chan.j@stu.janesville.k12.wi.us>
2023-01-16 22:58:35 -0600

    idk

---
+++ src/newstuff/Cl435g.java
@@ -0,0 +1,17 @@
+package newstuff;
+
+
+public class Cl435g {
+    private int myScore;
+    private int myRank;
+
+    public Cl435g(int score) {
+        myScore = score;
+        myRank = 0;
+    }
+
+    public int getScore() { return myScore; }
+    public int getRank() { return myRank; }
+    public void setRank(int rank) { myRank = rank; }
+    public String toString() { return myScore + "\t\t" + myRank; }
+}
